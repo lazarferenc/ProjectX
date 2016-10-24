b8b9f24c147668ee814478c2a510a888ea8f4475
Horvath Adrienn <akamaru8888@freemail.hu>
2016-10-24 15:16:19 +0200

    pozitcionalasok

--- MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    CheckBox szulo_checkbox;
+    CheckBox ovono_checkbox;
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btn_login);
+        szulo_checkbox=(CheckBox)findViewById(R.id.cb_szulo);
+        ovono_checkbox=(CheckBox)findViewById(R.id.cb_ovono);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*if(ovono_checkbox.getId()==0)*/
+
+                Intent intent=new Intent(MainActivity.this,OvonoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
