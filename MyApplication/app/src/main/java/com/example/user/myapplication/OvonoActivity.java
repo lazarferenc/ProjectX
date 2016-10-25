c896466d4f41cf17bd1de9a14f1d0e3f5d141fb5
lazarferenc <fecusrally@gmail.com>
2016-10-25 09:57:21 +0200

    added_jelenleti
    Hozza adtam egy jelenleti ablakot a projecthez es azon lefejlesztettem
    egy spinnert_delutan folytatom

--- MyApplication/app/src/main/java/com/example/user/myapplication/OvonoActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/OvonoActivity.java
@@ -1,13 +1,29 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class OvonoActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ovono);
+        btn=(Button)findViewById(R.id.btn_jelenleti);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent=new Intent(OvonoActivity.this,JelenletiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
+
