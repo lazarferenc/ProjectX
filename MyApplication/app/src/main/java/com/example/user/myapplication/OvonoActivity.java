5ae6ad0f56fd74b351dd21381b555c7e375936c9
lazarferenc <fecusrally@gmail.com>
2016-11-22 17:04:19 +0100

    update menu

--- MyApplication/app/src/main/java/com/example/user/myapplication/OvonoActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/OvonoActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class OvonoActivity extends AppCompatActivity {
-    Button btn;
+    Button btnjelenleti;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ovono);
-        btn=(Button)findViewById(R.id.btn_gyerekem);
+        btnjelenleti=(Button)findViewById(R.id.btn_gyerekem);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnjelenleti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                Intent intent=new Intent(OvonoActivity.this,JelenletiActivity.class);
+               Intent intent=new Intent(OvonoActivity.this,OvonoJelenletiActivity.class);
                 startActivity(intent);
             }
         });
