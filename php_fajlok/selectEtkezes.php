bcb15a17f68eff4388dde0374d0e51eddade8495
lazarferenc <fecusrally@gmail.com>
2016-12-02 17:28:16 +0100

    db table and php file add
    hozzáadva az étkezéses menü php fájla plus adatbázisba elkészítve

---
+++ php_fajlok/selectEtkezes.php
@@ -0,0 +1,28 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+	include 'connect.php';
+	selectEtkezes();
+}
+function selectEtkezes()
+{
+	global $con;
+	$query = "Select * From etkezes; ";
+	$result = mysqli_query($con,$query);
+	$number_of_rows = mysqli_num_rows($result);
+	$temp_array = array();
+	
+	if($number_of_rows > 0) {
+		while($row = mysqli_fetch_assoc($result)){
+			$temp_array[] = $row;
+			
+		}
+	}
+	header('Content-Type: application/json;charset=utf-8');
+	echo json_encode(array("etkezes"=>$temp_array));
+	mysqli_close($con);
+	
+}
+
+
+?>
